6ba9d60ad044d84f91b883f922cfa7ec70cfc2a2
kevincaires <kevincaires@ymail.com>
2022-07-24 18:11:40 -0300

    ADD drawing rectangle with mouse capture.

---
+++ drowning_rect.py
@@ -0,0 +1,29 @@
+"""
+Drawing rectangle in screen.
+"""
+from tools.utils import PyGame
+from settings.colors import Color
+from settings.config import *
+
+
+with PyGame() as pygame:
+    screen = pygame.display.set_mode(SCREEN_CONF)
+    start = (0, 0)
+    size = (0, 0)
+    drawing = False
+
+    while RUNNING:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                RUNNING = False
+            elif event.type == MOUSEBUTTONDOWN:
+                start = event.pos
+                size = 0, 0
+                drawing = True
+            elif event.type == MOUSEBUTTONUP:
+                end = event.pos
+                size = end[0] - start[0], end[1] - start[1]
+    
+        screen.fill(Color.grey)
+        pygame.draw.rect(screen, Color.red, (start, size), 2)
+        pygame.display.update()
